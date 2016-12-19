bbb98c4df023bc7d288cac0b4f4bbc07e077e6b0
ribaslucian <ribas.lucian@gmail.com>
2016-12-19 16:55:52 -0200

    Tent. de conf. de e-mail em produção.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # devise config
+  config.action_mailer.default_url_options = { host: 'rails-mystore.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: 'ribas.lucian@gmail.com',
+    password: ')8%9@8%5'
+  }
 end

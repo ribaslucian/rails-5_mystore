a951cd6813e67f9a64768b17405ef62cdbcf696c
ribaslucian <ribas.lucian@gmail.com>
2016-12-27 03:55:14 -0200

    Quase corrigindo o bug.

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,5 +5,11 @@ class Image < Model
                     styles: { medium: '300x300>', thumb: '100x100>' },
                     default_url: '/assets/original/generic_product.png'
 
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  # has_attached_file :image,
+  #   :path => ":rails_root/public/images/:id/:filename",
+  #   :url  => "/images/:id/:filename"
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/,
+  :reject_if => lambda { |t| t['avatar'].nil? }
+
 end
